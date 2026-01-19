70ea3794f745e73a306529d76f96b2d418962d82
prathamesh <prathamesh69bhavsar@gmail.com>
2026-01-19 12:58:28 +0530

    added the data file for the men

---
+++ src/assets/Data/men/menData/MenData.js
@@ -0,0 +1,65 @@
+const MenData = [
+  {
+    id: 1,
+    title: "Freedom: Brahmastra",
+    type: "Oversized T-Shirts",
+    price: 999,
+    img1: "../photos/isro1.png",
+    img2: "../photos/isro2.png",
+    img3: "../photos/isro3.png",
+    img4: "../photos/isro4.png",
+    img5: "../photos/isro5.png",
+    display: "../photos/display1.png"
+  },
+
+  {
+    id: 2,
+    title: "Avatar: Air Bender",
+    type: "Oversized T-Shirts",
+    price: 1099,
+    img1: "../photos/avatar1.png",
+    img2: "../photos/avatar2.png",
+    img3: "../photos/avatar3.png",
+    img4: "../photos/avatar4.png",
+    img5: "../photos/avatar5.png",
+    display: "../photos/display1.png"
+  },
+
+  {
+    id: 3,
+    title: "Batman: 3D Logo",
+    type: "T-Shrits",
+    price: 799,
+    img1: "../photos/batman1.png",
+    img2: "../photos/batman2.png",
+    display: "../photos/display2.png"
+  },
+
+  {
+    id: 4,
+    title: "TSS Originals: Trident",
+    type: "Oversized T-Shirts",
+    price: 1399,
+    img1: "../photos/aqua1.png",
+    img2: "../photos/aqua2.png",
+    img3: "../photos/aqua3.png",
+    img4: "../photos/aqua4.png",
+    img5: "../photos/aqua5.png",
+    display: "../photos/display2.png"
+  },
+
+  {
+    id: 5,
+    title: "Disney: No Worries",
+    type: "Oversized T-Shirts",
+    price: 999,
+    img1: "../photos/disney1.png",
+    img2: "../photos/disney2.png",
+    img3: "../photos/disney3.png",
+    img4: "../photos/disney4.png",
+    img5: "../photos/disney5.png",
+    display: "../photos/display1.png"
+  }
+];
+
+export default MenData;
