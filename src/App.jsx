5f04455d4a8feb8ef11d052ca81ac69f517e5744
kulkarnisnehal947 <kulkarnisnehal2212@gmail.com>
2026-01-19 14:39:56 +0530

    Made Categories and Filters dynamic for Men/Women pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,15 @@ import React from "react";
 import Navbar from "./components/public/Navbar";
 import Footer from "./components/public/Footer"
 import AppRoutes from "./routes/AppRoutes"
-import Categories from "./components/common/Categories";
+
 
 const App = () => {
   return (
     <>
       <Navbar />
-
-      <Routes>
-        
-        <Route path="/" element={<Navigate to="/men" />} />
-
-       
-        <Route path="/men" element={<Men />} />
-        <Route path="/women" element={<Women />} />
-        <Route path="/sneakers" element={<Sneakers />} />
-      </Routes>
-
-      <Footer/>
-     
-    </Router>
-
-    
+      <AppRoutes />
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Men.jsx
+++ src/pages/Men.jsx
@@ -1,5 +1,20 @@
+import React from "react";
 import MainSlider from "../components/public/MainSlider";
+import Categories from "../components/common/Categories";
+import NewCollection from "../components/sections/men/Newcollection";
+import Filters from "../components/common/Filters";
+import { menFilters } from "../data/menFilters";
+import { menCategories } from "../data/menCategories";
 
-export default function Men() {
-  return <MainSlider activeCategory="MEN" />;
-}
+const Men = () => {
+  return (
+    <div>
+      <MainSlider activeCategory="MEN" />
+      <NewCollection />
+       <Categories data={menCategories} />
+      <Filters filters={menFilters} />
+    </div>
+  );
+};
+
+export default Men;
