e1a538acecdcd554ca7735ec54c7c712a8f0696b
unknown <amrutak621@gmail.com>
2026-01-22 12:35:57 +0530

    changed route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 // import LoginSection from "../pages/Login";
 // import RegisterSection from "../pages/Register";
-// import WishlistDefault from "../components/public/WishlistDefault";
+
 import { BrowserRouter as Router, Navigate } from "react-router-dom";
 import Men from "../pages/Men";
 import Women from "../pages/Women";
@@ -12,35 +12,32 @@ import CartDefault from "../components/public/CartDefault";
 import Refund from "../components/common/Refund";
 import About from "../components/common/About";
 import ScrollToTop from "../components/ScrollToTop";
-import Navigation from "../components/common/Navigation"
+import WishlistDefault from "../components/public/WishlistDefault";
+import ProductDetailsPage from "../pages/ProductDetailsPage";
+
+
 function App() {
   return (
     <div>
-      <ScrollToTop/>
-    <Routes>
-      <Route path="/login" element={<LoginSection />} />
-      <Route path="/register" element={<RegisterSection />} />
-      <Route path="/" element={<Navigate to="/men" />} />
-      <Route path="/men" element={<Men />} />
-      <Route path="/women" element={<Women />} />
-      <Route path="/sneakers" element={<Sneakers />} />
+
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Navigate to="/men" />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/sneakers" element={<Sneakers />} />
         <Route path="cartdefault" element={<CartDefault />} />
-      <Route path="/wishlist" element={<WishlistDefault />} />
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/product/:id" element={<ProductDetailsPage />} />
-       <Route path="/category/:slug" element={<Navigation/>} />
+        <Route path="wishlistdefault" element={<WishlistDefault />} />
+        <Route path="/product/:category/:id" element={<ProductDetailsPage />} />
 
-
-       {/* routes to footer , do not change */}
+        {/* routes to footer , do not change */}
         <Route path="track" element={<Track />} />
         <Route path="contact" element={<Contact />} />
         <Route path="refund" element={<Refund />} />
         <Route path="about" element={<About />} />
-    </Routes>
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-
